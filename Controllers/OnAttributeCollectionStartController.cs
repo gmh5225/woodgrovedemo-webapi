91bb8c918e00958e32493bf3b722c3acf41caa9d
Yoel Hor <yoelhor@live.com>
2023-04-25 15:17:10 +0300

    Log

--- Controllers/OnAttributeCollectionStartController.cs
+++ Controllers/OnAttributeCollectionStartController.cs
@@ -23,7 +23,7 @@ public class OnAttributeCollectionStartController : ControllerBase
     {
         _logger.LogInformation("*********** OnAttributeCollectionStart ***********");
         string requestBody = await new StreamReader(this.Request.Body).ReadToEndAsync();
-        //_logger.LogInformation(requestBody);
+        _logger.LogInformation(requestBody);
 
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(requestBody));
         RequestData data = await JsonSerializer.DeserializeAsync<RequestData>(stream);
